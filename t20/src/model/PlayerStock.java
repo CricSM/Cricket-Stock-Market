10a2cf613be9f51830d010b9785a4edace747137
Aditya Jain <thecoderboy88@gmail.com>
2022-11-19 22:53:22 +0530

    Added Player Stock

---
+++ t20/src/model/PlayerStock.java
@@ -0,0 +1,9 @@
+package model;
+
+public class PlayerStock extends BaseBean {
+    private String stockId;
+    private String playerId;
+    private String noOfStock;
+    private String debutValue;
+    private String currentValue;
+}
